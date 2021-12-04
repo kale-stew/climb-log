a9c1d413b952e84452d16c8ef6ea0c3437cc99dc
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2021-12-04 15:44:48 -0700

    i hope these are in the right place?

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  const handleRouteChange = (url) => {
+    window.gtag('config', '[Tracking ID]', {
+      page_path: url,
+    })
+  }
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return <Component {...pageProps} />
 }
