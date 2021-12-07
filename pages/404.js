173ddbad9f762c87f895beb488c686ca176f39c5
Kylie Stewart <kylie@kyliestewart.tech>
2021-12-07 15:26:57 -0700

    🕵️‍♀️ Introduce NotFoundPage route

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+
+const NotFoundPage = () => (
+  <Layout>
+    <Head>
+      <title>Page Not Found | kylies.photos</title>
+    </Head>
+    <br />
+    <h2>Page Not Found</h2>
+  </Layout>
+)
+
+export default NotFoundPage
