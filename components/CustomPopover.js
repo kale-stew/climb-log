c3e85b598c988d1d2cd7ce6d1ca2e6afc9fe6e09
Kylie Stewart <kylie@kyliestewart.tech>
2021-12-08 18:21:11 -0700

    Update styles

--- components/CustomPopover.js
+++ components/CustomPopover.js
@@ -41,16 +41,16 @@ const CustomPopover = ({ climb, metric }) => {
       }
     >
       <header>
-        {buildTitle()}
         {climb.imgUrl && (
           <Image
             src={climb.imgUrl}
-            width="100%"
-            height="100%"
+            width="100"
+            height="100"
             layout="responsive"
             objectFit="contain"
           />
         )}
+        {buildTitle()}
       </header>
 
       <FormattedDate dateString={climb.date} withDOW />
