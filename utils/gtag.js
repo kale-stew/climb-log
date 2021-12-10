20792bde927162db556e5d019a0857ec495a904c
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2021-12-10 10:11:37 -0700

    this gets put into the HTML anyways?

--- utils/gtag.js
+++ utils/gtag.js
@@ -1,8 +1,6 @@
-export const GA_TRACKING_ID = process.env.PUBLIC_GA_ID
-
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-  window.gtag('config', GA_TRACKING_ID, {
+  window.gtag('config', 'G-W9WRKKHEN8', {
     page_path: url,
   })
 }
