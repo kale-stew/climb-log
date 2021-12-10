4c6fab5c13dbd39951f28196a4d91ec540f22e25
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2021-12-09 19:44:06 -0700

    trying this?

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
