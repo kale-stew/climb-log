113d8d88e6f39a0aace4f15490ebebe143adf19f
Kylie Stewart <kylie@kyliestewart.tech>
2021-12-31 23:01:11 -0700

    Styled links, mobile improvements

--- components/Card.js
+++ components/Card.js
@@ -6,26 +6,30 @@ import { PREVIEW_IMAGES } from '../utils/constants'
 import styles from './Card.module.css'
 
 const Card = ({ postData }) => (
-  <div className={styles.card}>
-    <div className={styles.headerImg}>
-      <Image
-        src={
-          postData.previewImgUrl ? postData.previewImgUrl : PREVIEW_IMAGES.FALLBACK_IMAGE
-        }
-        width={'100%'}
-        height={'100%'}
-        layout="responsive"
-      />
-    </div>
+  <Link href={postData.href}>
+    <div className={styles.card}>
+      <div className={styles.headerImg}>
+        <Image
+          src={
+            postData.previewImgUrl
+              ? postData.previewImgUrl
+              : PREVIEW_IMAGES.FALLBACK_IMAGE
+          }
+          width={'100%'}
+          height={'100%'}
+          layout="responsive"
+        />
+      </div>
 
-    <div className={styles.cardText}>
-      <h2 className={styles.cardTitle}>
-        <Link href={postData.href}>{postData.title}</Link>
-      </h2>
-      <FormattedDate dateString={postData.date} withDOW />
-      <p>{postData.description}</p>
+      <div className={styles.cardText}>
+        <h2 className={styles.cardTitle}>
+          <Link href={postData.href}>{postData.title}</Link>
+        </h2>
+        <FormattedDate dateString={postData.date} withDOW />
+        <p>{postData.description}</p>
+      </div>
     </div>
-  </div>
+  </Link>
 )
 
 export default Card
