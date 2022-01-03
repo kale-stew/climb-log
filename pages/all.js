ebcb676e86f4ce606b98243b2eb1ca64ceb6d3ca
Kylie Stewart <kylie@kyliestewart.tech>
2022-01-02 18:06:45 -0700

    🗓 Seperate images by calendar year

--- pages/all.js
+++ pages/all.js
@@ -1,16 +1,32 @@
 import ImageGallery from '../components/ImageGallery'
 import Layout from '../components/Layout'
 import { COLORS, METADATA, PREVIEW_IMAGES } from '../utils/constants'
+import { lightFormat } from 'date-fns'
 import { socialImage } from '../utils/social-image'
 
 import utilStyles from '../styles/utils.module.css'
 import { fetchAllImages } from '../utils/data/photos'
 
 export default function AllPhotosPage({ title, allPhotos }) {
+  const getAllYears = () => {
+    const arr = allPhotos.map(({ date }) => {
+      const year = lightFormat(new Date(date), 'y')
+      return year
+    })
+    return Array.from(new Set(arr))
+  }
+
+  const filterByYear = (arr, year) => arr.filter(({ date }) => date.indexOf(year) >= 0)
+
   return (
     <Layout>
       <h1 className={utilStyles.centerText}>{title}</h1>
-      <ImageGallery photos={allPhotos} />
+      {getAllYears().map((year) => {
+        const filteredPhotos = filterByYear(allPhotos, year)
+        return (
+          <ImageGallery key={`${year}-gallery`} photos={filteredPhotos} header={year} />
+        )
+      })}
     </Layout>
   )
 }
