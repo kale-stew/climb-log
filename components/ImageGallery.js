d0388158fcfaf2052293a1377b09d527348fab7d
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2022-01-04 15:30:18 -0700

    make taller images shrink to fit to see entire picture

--- components/ImageGallery.js
+++ components/ImageGallery.js
@@ -5,15 +5,26 @@ const ImageGallery = ({ header, photos }) => (
   <>
     {photos.length !== 0 && <h2 key={header}>{header}</h2>}
     <ul className={styles.galleryWrapper}>
-      {photos.map((photo) => (
-        <li key={photo.href} className={styles.galleryItem}>
-          <img src={photo.href} alt={photo.title} loading="lazy" />
-          <div className={`${styles.imageCaption} ${utilStyles.shownForMobile}`}>
-            <small>{photo.title}</small>
-            {photo.area && <small>{`📍${photo.area}, ${photo.state}`}</small>}
-          </div>
-        </li>
-      ))}
+      {photos.map((photo) => {
+        let tallImg = photo.height > photo.width
+        return (
+          <li
+            key={photo.href}
+            className={tallImg ? styles.tallGalleryItem : styles.galleryItem}
+          >
+            <img
+              src={photo.href}
+              alt={photo.title}
+              className={tallImg ? styles.tallGalleryImg : ''}
+              loading="lazy"
+            />
+            <div className={`${utilStyles.shownForMobile} ${styles.imageCaption}`}>
+              <small>{photo.title}</small>
+              {photo.area && <small>{`📍${photo.area}, ${photo.state}`}</small>}
+            </div>
+          </li>
+        )
+      })}
       <li key={'end-li'}></li>
     </ul>
   </>
