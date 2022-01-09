0f3446eac06fd9906d4260ce34b8a0d526b4865e
Kylie Stewart <kylie@kyliestewart.tech>
2022-01-09 11:14:45 -0700

    Comment out FacebookShareButton for now (I need an account to test fb links 🙃)

--- components/ShareButton.js
+++ components/ShareButton.js
@@ -1,36 +1,24 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import { FiFacebook, FiTwitter } from 'react-icons/fi'
+import { /*FiFacebook,*/ FiTwitter } from 'react-icons/fi'
 import { METADATA } from '../utils/constants'
 
-const FacebookShareButton = styled.button`
-  --color-fb-blue: #365aa9;
-  color: white;
-  box-shadow: inset 0 -2em 0 0 var(--color-fb-blue);
-  vertical-align: middle;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  gap: 0.25em;
-  padding: 0.2em 0.5em;
-  &:hover {
-    box-shadow: inset 0 -2em 0 0 var(--color-bg-primary);
-    color: var(--color-fb-blue);
-  }
-`
-const shareToFacebook = (href, text) => {
-  // to use:
-  //   const onClickTwitterIcon = (e) => {
-  //     e.preventDefault()
-  //     return shareToTwitter(window.location.href, text)
-  //   }
-  window.FB.ui({
-    method: 'share',
-    mobile_iframe: true,
-    href,
-    quote: text,
-  })
-}
+// const FacebookShareButton = styled.button`
+//   --color-fb-blue: #365aa9;
+//   color: white;
+//   box-shadow: inset 0 -2em 0 0 var(--color-fb-blue);
+//   vertical-align: middle;
+//   cursor: pointer;
+//   display: flex;
+//   align-items: center;
+//   gap: 0.25em;
+//   padding: 0.2em 0.5em;
+//   &:hover {
+//     box-shadow: inset 0 -2em 0 0 var(--color-bg-primary);
+//     color: var(--color-fb-blue);
+//   }
+// `
+
 const TwitterShareButton = styled.button`
   --color-t-blue: #5ba4d5;
   color: white;
@@ -46,12 +34,17 @@ const TwitterShareButton = styled.button`
     color: var(--color-t-blue);
   }
 `
+
+// const shareToFacebook = (href, text) => {
+//   window.FB.ui({
+//     method: 'share',
+//     mobile_iframe: true,
+//     href,
+//     quote: text,
+//   })
+// }
+
 const shareToTwitter = (href, text) => {
-  // to use:
-  //   const onClickFacebookIcon = (e) => {
-  //     e.preventDefault()
-  //     return shareToFacebook(window.location.href, text)
-  //   }
   window.open(
     `https://twitter.com/share?url=${encodeURI(encodeURI(href))}&text=${text}`,
     'sharer',
@@ -60,32 +53,39 @@ const shareToTwitter = (href, text) => {
 }
 
 const ShareButton = ({ type, data }) => {
+  const onClickTwitterIcon = (e, text) => {
+    e.preventDefault()
+    return shareToTwitter(window.location.href, text)
+  }
+
+  // const onClickFacebookIcon = (e, text) => {
+  //   e.preventDefault()
+  //   return shareToFacebook(window.location.href, text)
+  // }
+
   switch (type) {
+    // case 'facebook':
+    //   const fbTxt = `Read '${data.title}' by @${METADATA.FULL_NAME}`
+    //   return (
+    //     <>
+    //       <FacebookShareButton onClick={(e) => onClickFacebookIcon(e, fbTxt)}>
+    //         Share on <FiFacebook style={{ margin: 'auto 0' }} />
+    //         Facebook
+    //       </FacebookShareButton>
+    //     </>
+    //   )
     case 'twitter':
+      const twtTxt = `Read '${data.title}' by @${METADATA.TWITTER_HANDLE}`
       return (
         <>
-          <TwitterShareButton
-            href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-            data-text={`Read '${data.title}' on ${METADATA.SITE_NAME} by @${METADATA.TWITTER_HANDLE}`}
-            data-url={data.href}
-          >
+          <TwitterShareButton onClick={(e) => onClickTwitterIcon(e, twtTxt)}>
             Share on <FiTwitter style={{ margin: 'auto 0' }} />
             Twitter
           </TwitterShareButton>
         </>
       )
-    case 'facebook':
-      return (
-        <>
-          <FacebookShareButton
-            data-text={`Read '${data.title}' on ${METADATA.SITE_NAME} by @${METADATA.FULL_NAME}`}
-            data-url={data.href}
-          >
-            Share on <FiFacebook style={{ margin: 'auto 0' }} />
-            Facebook
-          </FacebookShareButton>
-        </>
-      )
+    default:
+      return null
   }
 }
 
