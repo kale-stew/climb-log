a229dbb216035d2397ff09bd785dcae93a60adef
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2022-01-08 19:28:26 -0700

    slight refactor and implament search gear without styles

--- pages/gear.js
+++ pages/gear.js
@@ -6,32 +6,79 @@ import { socialImage } from '../utils/social-image'
 
 import styles from '../styles/gear.module.css'
 import utilStyles from '../styles/utils.module.css'
+import { useEffect, useState } from 'react'
 
 const GearPage = ({ title, allGear }) => {
+  const [gearCategories, setGearCategories] = useState()
+  const [pureGear, setPure] = useState()
+  const [gearData, setGearData] = useState()
+
+  const buildCategories = (gearList = pureGear, setState = false) => {
+    const arr = gearList.map((gear) => gear.category)
+    const returnArray = Array.from(new Set(arr))
+
+    if (setState) {
+      setGearCategories(returnArray)
+    }
+    return returnArray
+  }
+
+  const userSearch = (query) => {
+    const upperQuery = query.toUpperCase().trim()
+    if (upperQuery == '') {
+      buildCategories(pureGear, true)
+      setGearData(pureGear)
+      return
+    }
+    let filteredGear = pureGear.filter((gear) => {
+      let booleanVal =
+        gear.title?.toUpperCase().includes(upperQuery) ||
+        gear.category?.toUpperCase().includes(upperQuery) ||
+        gear.brand?.toUpperCase().includes(upperQuery) ||
+        gear.product_str?.toUpperCase().includes(upperQuery)
+      return booleanVal
+    })
+    buildCategories(filteredGear, true)
+    setGearData(filteredGear)
+  }
+
+  useEffect(() => {
+    setPure(allGear)
+    setGearData(allGear)
+    buildCategories(allGear, true)
+  }, [])
+
   const filterByCategory = (arr, cat) => arr.filter(({ category }) => category === cat)
   const createGearTitle = (item) =>
     capitalizeEachWord(
       `${item.brand}${item.product_str ? ` ${item.product_str} ` : ' '}${item.title}`
     )
-  const getGearCategories = () => {
-    const arr = allGear.map((gear) => gear.category)
-    return Array.from(new Set(arr))
-  }
 
   return (
     <Layout>
       <h1 className={utilStyles.centerText}>{title}</h1>
+      <div>
+        <input
+          // className={styles.searchInput} TODO STYLES
+          type={'search'}
+          placeholder="Try 'Helmet' or 'Backpacking'"
+          onChange={(e) => {
+            userSearch(e.target.value)
+          }}
+        />
+      </div>
       <div className={styles.gearWrapper}>
-        {getGearCategories().map((cat) => (
-          <>
-            <h3>{cat}</h3>
-            <ul>
-              {filterByCategory(allGear, cat).map((item) => (
-                <li key={item.title}>{createGearTitle(item)}</li>
-              ))}
-            </ul>
-          </>
-        ))}
+        {gearCategories &&
+          gearCategories.map((cat) => (
+            <>
+              <h3 key={`h3-${cat}`}>{cat}</h3>
+              <ul key={`ul-${cat}`}>
+                {filterByCategory(gearData, cat).map((item) => (
+                  <li key={`${cat}-${item.id}`}>{createGearTitle(item)}</li>
+                ))}
+              </ul>
+            </>
+          ))}
       </div>
     </Layout>
   )
