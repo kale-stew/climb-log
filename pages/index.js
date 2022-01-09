5bc0c6fbcc1c230adf99f26f570823b20113ea9c
Kylie Stewart <kylie@kyliestewart.tech>
2022-01-08 20:18:41 -0700

    🕶 Add dark mode (#80)
    * Init ThemeToggle & default theme styles
    
    * Ugh bg-image for faded landing image

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,22 @@
 import Card from '../components/Card'
 import Layout from '../components/Layout'
-import ResponsiveImage from '../components/ResponsiveImage'
 import { COLORS, METADATA, PREVIEW_IMAGES } from '../utils/constants'
 import { getRecentPosts } from '../utils/data/posts'
 import { socialImage } from '../utils/social-image'
-
-// Photos
-import LanderTop from '../public/photos/lander_top.png'
-import LanderBottom from '../public/photos/lander_bottom.png'
-import MobileLanderTop from '../public/photos/Mlander_top.png'
-import MobileLanderBottom from '../public/photos/Mlander_bottom.png'
 
 import styles from '../components/Layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 
 const HomePage = ({ featuredPosts }) => (
   <Layout home>
-    <ResponsiveImage
-      altTxt="Looking through fall colors towards Capitol Peak in Aspen, Colorado."
-      desktopImg={LanderTop}
-      desktopDimensions={{ width: 2100, height: 824 }}
-      mobileImg={MobileLanderTop}
-      mobileDimensions={{ width: 1200, height: 550 }}
-    />
-
+    <div className={utilStyles.gradientTop} />
     <h1 className={`${utilStyles.heading2Xl} ${utilStyles.centerText}`}>Recent Posts</h1>
     <div className={styles.recentPosts}>
       {featuredPosts.map((post) => (
         <Card postData={post} key={post.id} />
       ))}
     </div>
-
-    <ResponsiveImage
-      altTxt="Looking through fall colors towards Capitol Peak in Aspen, Colorado."
-      desktopImg={LanderBottom}
-      desktopDimensions={{ width: 2100, height: 888 }}
-      mobileImg={MobileLanderBottom}
-      mobileDimensions={{ width: 1200, height: 611 }}
-    />
+    <div className={utilStyles.gradientBottom} />
   </Layout>
 )
 
