1c23260bf2e2e547601f9f5c6453205e0aca3d7b
Kylie Stewart <kylie@kyliestewart.tech>
2022-01-09 13:40:43 -0700

    Small style improvements

--- components/ThemeToggle.js
+++ components/ThemeToggle.js
@@ -1,45 +1,66 @@
 import styled from '@emotion/styled'
+import { keyframes } from '@emotion/react'
 import { useState, useEffect } from 'react'
 import { BsMoonStars, BsSun } from 'react-icons/bs'
 
-import styles from './Footer.module.css'
+const bounce = keyframes`
+0% {
+  transform: translate(0);
+}
+25% {
+  transform: translate(0, -4px);
+}
+75% {
+  transform: translate(0, 8px);
+}
+100% {
+  transform: translate(0);
+}
+`
+
+const spin = keyframes`
+0% {
+  transform: rotate(-120deg);
+}
+50% {
+  transform: rotate(90deg);
+}
+100% {
+  transform: rotate(0deg);
+}
+`
 
 const ToggleButton = styled.button`
-  --toggle-width: 40px;
-  --toggle-height: 20px;
-  position: relative;
+  background: transparent;
+  color: var(--color-text-primary);
+  width: 34px;
+  height: 17px;
   display: flex;
   align-items: center;
-  justify-content: space-around;
-  font-size: 1.5rem;
-  padding: 1.5rem;
-  margin-bottom: 1rem;
-  width: var(--toggle-width);
-  height: var(--toggle-height);
-  border: 1px solid var(--color-text-primary);
-  border-radius: 50%;
+  justify-content: center;
+  font-size: 1.4rem;
+  padding: 1.22rem;
   cursor: pointer;
-  background: var(--color-bg-primary);
-  color: var(--color-text-primary);
-  transition: background 0.25s ease-in-out, box-shadow 0.25s ease-in-out;
-  &:focus {
-    outline-offset: 5px;
-  }
-  &:focus:not(:focus-visible) {
-    outline: none;
-  }
+  transition: background 0.25s ease-in-out;
   &:hover {
-    background: var(--color-bg-tertiary);
+    color: var(--color-text-primary);
+    background-color: transparent;
+    transform-origin: none;
   }
 `
 
-const TogglePoint = styled.span`
-  position: absolute;
-  transition: transform 0.25s ease-in-out;
-  transform: ${(p) =>
-    p.activeTheme === 'dark'
-      ? 'translate3d(calc(var(--toggle-width) - var(--toggle-height)), 0, 0)'
-      : 'none'};
+const MoonSpan = styled.span`
+  &:hover {
+    animation: ${bounce} 0.75s ease-in-out;
+  }
+`
+
+const SunSpan = styled.span`
+  margin: 0;
+  padding: 0;
+  &:hover {
+    animation: ${spin} 0.6s ease-in-out;
+  }
 `
 
 const ThemeToggle = () => {
@@ -51,18 +72,15 @@ const ThemeToggle = () => {
   }, [activeTheme])
 
   return (
-    <div className={styles.toggleWrapper}>
-      <ToggleButton
-        aria-label={`Change to ${inactiveTheme} mode`}
-        title={`Change to ${inactiveTheme} mode`}
-        type="button"
-        onClick={() => setActiveTheme(inactiveTheme)}
-      >
-        <TogglePoint activeTheme={activeTheme} />
-        <span>{activeTheme === 'dark' && <BsMoonStars />}</span>
-        <span>{activeTheme === 'light' && <BsSun />}</span>
-      </ToggleButton>
-    </div>
+    <ToggleButton
+      aria-label={`Switch to ${inactiveTheme} mode`}
+      title={`Switch to ${inactiveTheme} mode`}
+      type="button"
+      onClick={() => setActiveTheme(inactiveTheme)}
+    >
+      <MoonSpan>{activeTheme === 'dark' && <BsMoonStars />}</MoonSpan>
+      <SunSpan>{activeTheme === 'light' && <BsSun />}</SunSpan>
+    </ToggleButton>
   )
 }
 
