7e50f7e8c51c64966202e60e87f1d9473d5a78d7
Kylie Stewart <kylie@kyliestewart.tech>
2022-01-09 14:36:45 -0700

    Use styled to co-locate landing images

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled'
 import Card from '../components/Card'
 import Layout from '../components/Layout'
 import { COLORS, METADATA, PREVIEW_IMAGES } from '../utils/constants'
@@ -7,16 +8,36 @@ import { socialImage } from '../utils/social-image'
 import styles from '../components/Layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 
+const GradientTopImage = styled.div`
+  background-image: linear-gradient(transparent, var(--color-bg-primary)),
+    url('/photos/lander_top.jpg');
+  height: 30vh;
+  width: 100%;
+  background-size: cover;
+  @media (max-width: 1024px) {
+    max-height: 25vh;
+  }
+`
+
+const GradientBottomImage = styled.div`
+  background-image: linear-gradient(var(--color-bg-primary), transparent),
+    url('/photos/lander_bottom.jpg');
+  height: 25vh;
+  width: 100%;
+  background-size: cover;
+  margin-top: 5vh;
+`
+
 const HomePage = ({ featuredPosts }) => (
   <Layout home>
-    <div className={utilStyles.gradientTop} />
+    <GradientTopImage />
     <h1 className={`${utilStyles.heading2Xl} ${utilStyles.centerText}`}>Recent Posts</h1>
     <div className={styles.recentPosts}>
       {featuredPosts.map((post) => (
         <Card postData={post} key={post.id} />
       ))}
     </div>
-    <div className={utilStyles.gradientBottom} />
+    <GradientBottomImage />
   </Layout>
 )
 
