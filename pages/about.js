2ea5b02ad6df8425ddc485d4dc5569d2582f1d48
kale-stew <kylie@kyliestewart.tech>
2022-09-07 20:58:19 -0600

    🧹 Remove excess padding

--- pages/about.js
+++ pages/about.js
@@ -95,7 +95,7 @@ const AboutPage = ({ personalInfo, title }) => {
                   style={{ borderRadius: '12px', marginTop: '1rem' }}
                   src={`${interview.embedHref}?utm_source=generator&theme=0`}
                   width="100%"
-                  height="150"
+                  height="80"
                   frameBorder="0"
                   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 ></iframe>
