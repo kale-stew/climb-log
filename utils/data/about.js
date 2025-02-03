119743af5de142d96747abbaab5cd34aefa2394b
Kylie Czajkowski <hello@kylieis.online>
2025-02-02 20:54:55 -0800

    about page updates

--- utils/data/about.js
+++ utils/data/about.js
@@ -1,21 +1,9 @@
 const brands = [
   {
-    name: 'Alder Apparel',
-    href: 'https://glnk.io/jxww/kalestewski',
-    description:
-      'Alder offers inclusive sizing, community-informed design, sustainable and ethical production and a playful brand that starkly contrasts with the performance-driven, hard-core athletic brands that currently dominate the outdoor space. Use code KYLIE10 for 10% off your purchase!',
-  },
-  {
-    name: 'Goodr',
-    href: 'https://goodr.com',
-    description:
-      'Extremely functional and affordable nonslip sunglasses made to be worn everywhere, from high peaks to the dog park to the local brewery.',
-  },
-  {
     name: 'Notion.so',
     href: 'https://notion.so',
     description:
-      'A productivity app used for everything from personal note-taking to professional project management work. Fun fact: the climb log on this website is built using it!',
+      'A productivity app used for everything from personal note-taking to professional project management work. Fun fact: It powers almost every database used on this website. ✨',
   },
   {
     name: 'Keep Nature Wild',
@@ -23,6 +11,12 @@ const brands = [
     description:
       'An eco-minded outdoor brand that pledges to pick up 1lb of trash for every product sold.',
   },
+  // {
+  //   name: 'Goodr',
+  //   href: 'https://goodr.com',
+  //   description:
+  //     'Extremely functional and affordable nonslip sunglasses made to be worn everywhere, from high peaks to the dog park to the local brewery.',
+  // },
 ]
 
 const interviews = [
@@ -49,6 +43,14 @@ const photos = [
       'Kylie and Kyle near their camp in the Longs boulderfield, in Rocky Mountain National Park. Chasm Lake is behind them.',
   },
   {
+    src: `${photoPrefix}halo-ridge.jpeg`,
+    thumbnail: `${photoPrefix}halo-ridge.jpeg`,
+    thumbnailWidth: 300,
+    thumbnailHeight: 400,
+    caption:
+      'Kylie looking at Mount of the Holy Cross from the Halo Ridge approach route.',
+  },
+  {
     src: 'https://live.staticflickr.com/65535/51795954743_fc94da17ee_c.jpg',
     thumbnail: 'https://live.staticflickr.com/65535/51795954743_fc94da17ee_c.jpg',
     thumbnailWidth: 246,
@@ -64,35 +66,11 @@ const photos = [
       'Otis, taking a break while on a walk through their old neighborhood in downtown Denver.',
   },
   {
-    src: `${photoPrefix}kyle.jpeg`,
-    thumbnail: `${photoPrefix}kyle.jpeg`,
-    thumbnailWidth: 400,
-    thumbnailHeight: 300,
-    caption: 'Hiking near a campsite in Escalante, Utah.',
-  },
-  {
-    src: 'https://live.staticflickr.com/65535/51854601981_0fb24a6662_b.jpg',
-    thumbnail: 'https://live.staticflickr.com/65535/51854601981_0fb24a6662_b.jpg',
-    thumbnailWidth: 400,
-    thumbnailHeight: 259,
-    caption:
-      "Kylie sitting on the summit of Mount Lady Washington, Longs Peak's face visible in the background.",
-  },
-  {
-    src: `${photoPrefix}halo-ridge.jpeg`,
-    thumbnail: `${photoPrefix}halo-ridge.jpeg`,
-    thumbnailWidth: 300,
-    thumbnailHeight: 400,
-    caption:
-      'Kylie looking at Mount of the Holy Cross from the Halo Ridge approach route.',
-  },
-  {
-    src: 'https://live.staticflickr.com/65535/51854998124_8bd3217bca_b.jpg',
-    thumbnail: 'https://live.staticflickr.com/65535/51854998124_8bd3217bca_b.jpg',
-    thumbnailWidth: 246,
-    thumbnailHeight: 341,
-    caption:
-      'Kyle and Otis walking on a frozen Crater Lake in front of the Maroon Bells.',
+    src: `https://live.staticflickr.com/65535/51857457747_79996b28b5_k.jpg`,
+    thumbnail: `https://live.staticflickr.com/65535/51857457747_79996b28b5_k.jpg`,
+    thumbnailWidth: 240,
+    thumbnailHeight: 152,
+    caption: 'Descending a boulderfield on the route back down Pyramid Peak.',
   },
 ]
 
